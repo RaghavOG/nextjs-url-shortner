b040e5b6ca530313fa1739f9337cb84f78d3f71f
RaghavOG <04raghavsingla28@gmail.com>
2025-01-12 18:56:11 +0530

    deployment changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: [
-      "api.microlink.io", // Microlink Image Preview
-    ],
-  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
